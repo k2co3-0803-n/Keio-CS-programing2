c75020695ff5a418561b74c5db21c9f7ac57e9cf
ckosuke <k2co3.0803.n@icloud.com>
2024-07-14 11:25:08 +0900

    追加画面作成

--- RegisterFormLecture.java
+++ RegisterFormLecture.java
@@ -3,40 +3,81 @@ import java.awt.event.*;
 import javax.swing.*;
 
 // Here, define UI of Register form of new Lectures
-public class RegisterFormLecture {
-	private static String labelPrefix = "New String: ";
-	private JLabel label;
-	private JTextField text;
+public class RegisterFormLecture extends JFrame {
+	JTextField title;
+	JTextField id;
+	JTextField lectureName;
+	JTextField classRoom;
+	JTextField dayAndTime;
+	JTextField professor;
 
 	class ButtonAction implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			label.setText(labelPrefix + text.getText());
+			
 		}
 	}
-	public Component createComponents() {
-		JButton button = new JButton("Move!");
-		label = new JLabel(labelPrefix);
-		text = new JTextField();
 
+	// Constructor
+	public RegisterFormLecture(String frameName) {
+		JLabel titleLabel = new JLabel("新しい講義を追加");
+		JLabel idLabel = new JLabel("講義ID");
+		JLabel lectureNameLabel = new JLabel("講義名");
+		JLabel classRoomLabel = new JLabel("講義教室");
+		JLabel dayAndTimeLabel = new JLabel("講義時間");
+		JLabel professorLabel = new JLabel("担当教員");
+
+		id = new JTextField();
+		lectureName = new JTextField();
+		classRoom = new JTextField();
+		dayAndTime = new JTextField();
+		professor = new JTextField();
+
+
+		JButton addButton = new JButton("追加");
 		ButtonAction buttonListener = new ButtonAction();
-		button.addActionListener(buttonListener);
+		addButton.addActionListener(buttonListener);
 
-		JPanel pane = new JPanel();
-		pane.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-		pane.setLayout(new GridLayout(0, 1));
-		pane.add(text);
-		pane.add(button);
-		pane.add(label);
+		JPanel pane1 = new JPanel();
 
-		return pane;
+		pane1.add(titleLabel);
+		JPanel pane2 = new JPanel();
+		pane2.setLayout(new GridLayout(1, 0));
+		pane2.add(idLabel);
+		pane2.add(id);
+		JPanel pane3 = new JPanel();
+		pane3.setLayout(new GridLayout(1, 0));
+		pane3.add(lectureNameLabel);
+		pane3.add(lectureName);
+		JPanel pane4 = new JPanel();
+		pane4.setLayout(new GridLayout(1, 0));
+		pane4.add(classRoomLabel);
+		pane4.add(classRoom);
+		JPanel pane5 = new JPanel();
+		pane5.setLayout(new GridLayout(1, 0));
+		pane5.add(dayAndTimeLabel);
+		pane5.add(dayAndTime);
+		JPanel pane6 = new JPanel();
+		pane6.setLayout(new GridLayout(1, 0));
+		pane6.add(professorLabel);
+		pane6.add(professor);
+		JPanel mainPane = new JPanel();
+		mainPane.setLayout(new GridLayout(0, 1));
+		mainPane.add(pane1);
+		mainPane.add(pane2);
+		mainPane.add(pane3);
+		mainPane.add(pane4);
+		mainPane.add(pane5);
+		mainPane.add(pane6);
+		mainPane.add(addButton);
+		//mainPane.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+
+		this.setTitle(frameName);
+		this.getContentPane().add(mainPane, BorderLayout.CENTER);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(500, 600);
 	}
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Prob103");
-		Prob103 app = new Prob103();
-		Component contents = app.createComponents();
-		frame.getContentPane().add(contents, BorderLayout.CENTER);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500, 600);
-		frame.setVisible(true);
+		RegisterFormLecture registerFormLecture = new RegisterFormLecture("新しい講義を追加");
+		registerFormLecture.setVisible(true);
 	}
 }
